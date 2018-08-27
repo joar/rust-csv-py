a71016cbe5922aa16347bf76d1e335ec7fddb592
Joar Wandborg <joar@wandborg.se>
2018-08-27 14:27:02 +0200

    Set cmp and frozen to True
    So that __hash__ can be generated, this should fix this error:
    https://travis-ci.com/joar/rust-csv-py/builds/82952168

--- rustcsv/error.py
+++ rustcsv/error.py
@@ -2,7 +2,7 @@ from typing import Optional
 import attr
 
 
-@attr.s(slots=True, auto_attribs=True)
+@attr.s(slots=True, auto_attribs=True, cmp=True, frozen=True)
 class Position:
     byte: int
     line: int
@@ -13,13 +13,13 @@ class CSVError(Exception):
     pass
 
 
-@attr.s(auto_attribs=True)
+@attr.s(auto_attribs=True, cmp=True, frozen=True)
 class UTF8Error(CSVError):
     message: str
     position: Optional[Position] = None
 
 
-@attr.s(auto_attribs=True)
+@attr.s(auto_attribs=True, cmp=True, frozen=True)
 class UnequalLengthsError(CSVError):
     message: str
     position: Optional[Position] = None
