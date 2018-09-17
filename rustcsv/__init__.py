80e82e2993db34e5f40ece5a05d8b41b98b6c4e6
Joar Wandborg <joar@wandborg.se>
2018-09-17 14:34:19 +0200

    Avoid subclassing
    Until https://github.com/PyO3/pyo3/issues/220 is sorted out

--- rustcsv/__init__.py
+++ rustcsv/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 from typing import Union, BinaryIO
 
-from .rustcsv import CSVReader as _RustCSVReader, CSVWriter, __build__
+from .rustcsv import CSVReader, CSVWriter, __build__
 
 try:
     from ._version import version
@@ -12,28 +12,31 @@ except ImportError:
 __all__ = ["CSVReader", "CSVWriter", "__build__", "version"]
 
 
-class CSVReader(_RustCSVReader):
-    def __new__(
-        cls,
-        source: Union[BinaryIO, str],
-        delimiter: bytes = b",",
-        terminator: bytes = b"\n",
-    ):
-        """
-
-        Parameters
-        ----------
-        source
-            :any:`binary file` or string to read CSV from.
-        delimiter
-            Byte to use as CSV field delimiter
-        terminator
-            Byte to use as CSV record terminator
-
-        Returns
-        -------
-        CSVReader
-        """
-        return super(CSVReader, cls).__new__(
-            cls, path_or_fd=source, delimiter=delimiter, terminator=terminator
-        )
+# Can't subclass for better docstrings: https://github.com/PyO3/pyo3/issues/220
+# class CSVReader(_RustCSVReader):
+#     def __new__(
+#         cls,
+#         source: Union[BinaryIO, str],
+#         delimiter: bytes = b",",
+#         terminator: bytes = b"\n",
+#     ):
+#         """
+#
+#         Parameters
+#         ----------
+#         source
+#             :any:`binary file` or string to read CSV from.
+#         delimiter
+#             Byte to use as CSV field delimiter
+#         terminator
+#             Byte to use as CSV record terminator
+#
+#         Returns
+#         -------
+#         CSVReader
+#         """
+#         self = super(CSVReader, cls).__new__(
+#             cls, path_or_fd=source, delimiter=delimiter, terminator=terminator
+#         )
+#
+#         return self
