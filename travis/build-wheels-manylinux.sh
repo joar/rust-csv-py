b63da800ed7ae8b03583a24d0ab962a0ed6857ec
Joar Wandborg <joar@wandborg.se>
2018-12-17 16:56:51 +0100

    Update build scripts

--- travis/build-wheels-manylinux.sh
+++ travis/build-wheels-manylinux.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 # script directory, e.g. "<git-repo>/travis/"
 TRAVIS_DIR="$(dirname "${BASH_SOURCE[0]}")"
-# shellcheck source=travis/_output_helpers.sh
-source "$TRAVIS_DIR/_output_helpers.sh"
+# BEGIN UTILS
+# Output utilities - DO NOT EDIT - Automatically inserted by travis/utils.sh
+# ==============================================================================
+function green { printf "\x1b[32m%s\x1b[0m\n" "$@" >&2; }
+function red { printf "\x1b[31m%s\x1b[0m\n" "$@" >&2; }
+function yellow { printf "\x1b[33m%s\x1b[0m\n" "$@" >&2; }
+function message { echo "$@" >&2; } # like echo, but prints to stderr
+# END UTILS
 
 # Install a system package required by our library
 # yum install -y openssl-devel
 
 build_wheels()  {
     "$TRAVIS_DIR"/install-rust.sh
-    export PATH="$HOME/.cargo/bin:$PATH"
+    CARGO_BIN="$HOME/.cargo/bin"
+    green "Adding $CARGO_BIN to \$PATH"
+    export PATH="$CARGO_BIN:$PATH"
 
     # Parameters
     WHEELHOUSE="${WHEELHOUSE:-"/io/wheelhouse"}"
@@ -76,9 +84,5 @@ build_wheel() {
     env "${env_vars[@]}" "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 }
 
-if [[ "${BASH_SOURCE[0]}" = "${0}" ]]; then
-    set -e -x
-    build_wheels "$@"
-else
-    yellow "Script was sourced, not executing build_wheels"
-fi
+(set -e -x;
+build_wheels "$@")
--- travis/install-rust.sh
+++ travis/install-rust.sh
@@ -1,32 +1,26 @@
 #!/bin/bash
 # script directory, e.g. "<git-repo>/travis/"
 TRAVIS_DIR="$(dirname "${BASH_SOURCE[0]}")"
-# shellcheck source=travis/_output_helpers.sh
-source "$TRAVIS_DIR/_output_helpers.sh"
+
+# BEGIN UTILS
+# Output utilities - DO NOT EDIT - Automatically inserted by travis/utils.sh
+# ==============================================================================
+function green { printf "\x1b[32m%s\x1b[0m\n" "$@" >&2; }
+function red { printf "\x1b[31m%s\x1b[0m\n" "$@" >&2; }
+function yellow { printf "\x1b[33m%s\x1b[0m\n" "$@" >&2; }
+function message { echo "$@" >&2; } # like echo, but prints to stderr
+# END UTILS
 
 # Parameters
+# ==============================================================================
 RUSTCSV_RUST_VERSION="${RUSTCSV_RUST_VERSION:-"nightly"}"
 
-install_rust() {
-    # install rust + cargo
-    # ============================
-    CARGO_BIN=$HOME/.cargo/bin
-    if ! test -d "$CARGO_BIN"; then
-        green "Installing rust + cargo, version $RUSTCSV_RUST_VERSION"
-        curl https://sh.rustup.rs -sSf \
-            | sh -s -- -y --default-toolchain "$RUSTCSV_RUST_VERSION"
-    fi
-    if ! grep "$CARGO_BIN" <<<"$PATH" &> /dev/null; then
-        green "Adding $CARGO_BIN to \$PATH"
-        export PATH="$CARGO_BIN:$PATH"
-    fi
-}
-
-
-if [[ "${BASH_SOURCE[0]}" = "${0}" ]]; then
-    red "The script should be sourced so that it's able to export \$PATH." \
-        "e.g. 'source ${BASH_SOURCE[*]}' will install rust and re-export \$PATH"
-    exit 1
-else
-    install_rust "$@"
+# install rust + cargo
+# ==============================================================================
+CARGO_BIN=$HOME/.cargo/bin
+green "Installing rust + cargo, default toolchain: $RUSTCSV_RUST_VERSION"
+curl https://sh.rustup.rs -sSf \
+    | sh -s -- -y --default-toolchain "$RUSTCSV_RUST_VERSION"
+if ! grep "$CARGO_BIN" <<<"$PATH" &> /dev/null; then
+    yellow "Warning: $CARGO_BIN is not in your \$PATH"
 fi
